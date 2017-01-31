466211c01c88171a1700083638dd30b859ee53db
Bobster94 <Bobstr@192.168.2.100>
2017-01-31 23:08:40 +0100

    opschonen / namen verandert

--- Main.py
+++ Main.py
@@ -2,6 +2,6 @@ import engine.DriveControls as controls
 import engine.config.Configuration as cfg
 import engine.GPIO as GPIO
 import time
-import scripts.LineFollower as script
+import scripts.CarNavigation as script
 
 script.start()
