b51e369e57f2fb6ab7ee70e1100d4e674be52d16
Mintal <matthijs1995@hotmail.com>
2017-01-31 14:12:49 +0100

    added old scripts

--- engine/GPIO.py
+++ engine/GPIO.py
@@ -3,6 +3,8 @@ import engine.config.Configuration as cfg
 import util.logger as log
 
 
+log.logger.info("finalized initializing GPIO")
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -18,13 +20,17 @@ GPIO.setup(cfg.pinmap['lineFollower'], GPIO.IN)
 GPIO.setup(cfg.pinmap['trigger'], GPIO.OUT)
 GPIO.setup(cfg.pinmap['echo'], GPIO.IN)
 
-#The frequency of power sent by the battery to the specified wheels, frequency is defined in: config>Configuration
 pwmMotorAF = GPIO.PWM(cfg.pinmap['motorAF'], cfg.defaultValues['frequency'])
 pwmMotorAB = GPIO.PWM(cfg.pinmap['motorAB'], cfg.defaultValues['frequency'])
-pwmMotorBF = GPIO.PWM(cfg.pinmap['motorBF'], cfg.defaultValues['freqcorrectionAF'])
+pwmMotorBF = GPIO.PWM(cfg.pinmap['motorBF'], cfg.defaultValues['frequency'])
 pwmMotorBB = GPIO.PWM(cfg.pinmap['motorBB'], cfg.defaultValues['frequency'])
 
-log.logger.info("finalized initializing GPIO")
+pwmMotorAF.start(cfg.pinmap['motorAF'])
+pwmMotorAB.start(cfg.pinmap['motorAB'])
+pwmMotorBF.start(cfg.pinmap['motorBF'])
+pwmMotorBB.start(cfg.pinmap['motorBB'])
 
 def clear():
     GPIO.cleanup()
+    
+#controls.driveForward()
